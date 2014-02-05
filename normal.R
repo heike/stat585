c84271ac9b7350bad5c76fe1d39ce248a5a0681c
unknown <jwillers@ts2-stat.iastate.edu>
2014-02-05 15:21:42 -0600

    Changed the mean once again from 65 to 50.

--- normal.R
+++ normal.R
@@ -4,5 +4,5 @@ qqnorm(mydata)
 
 #We can also change the mean and standard deviation. Suppose we want to simulate a sample of women's heights,
   #which are known to have a mean of 65 inches and standard deviation of 3.5 inches.
-heights<-rnorm(100,mean=60,sd=3.5)
+heights<-rnorm(100,mean=50,sd=3.5)
 
